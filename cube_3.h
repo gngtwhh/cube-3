94080cb6c1ecd7061121caf452bf683ed5ff2f42
gngtwhh <2784126850@qq.com>
2023-09-04 09:50:49 +0800

    create project

---
+++ cube_3.h
@@ -0,0 +1,8 @@
+//
+// Created by gngtwhh on 2023/9/4.
+//
+
+#ifndef CUBE_3_CUBE_3_H
+#define CUBE_3_CUBE_3_H
+
+#endif //CUBE_3_CUBE_3_H
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "cube_3.h"
+int main() {
+    init();
+    return 0;
+}
